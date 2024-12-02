1110ca603747e19d5274e54e725b1b1e0dcc44f6
marazp <156416841+marazp@users.noreply.github.com>
2024-12-02 13:54:31 +0100

    Moved the create trees to its own function

--- Huffman_old.cpp
+++ Huffman_old.cpp
@@ -8,17 +8,33 @@
 using namespace std;
 
 unordered_map<char, int> letterCounter(string input);
-TreeWrapper createTrees(const unordered_map<char, int>& letterCounted);
 
 int main() {
 
     string input = "aaaabbbcddhhiiiii";
 
     unordered_map<char, int> letterCounted = letterCounter(input);
-    TreeWrapper queue = createTrees(letterCounted);
+
+    priority_queue<TreeWrapper> q;
+
+    // Create base trees and put them in a queue
+    for(const auto & pair: letterCounted) {
+        q.push(TreeWrapper(new Tree(pair.second, pair.first)));
+    }
+
+    while (q.size() > 1) {
+        Tree* t1 = q.top().tree; 
+        q.pop();
+        Tree* t2 = q.top().tree;
+        q.pop();
+
+        //Merge first and second spot in queue
+        int weight = t1->getWeight() + t2->getWeight();
+        q.push(TreeWrapper(new Tree(weight, t1, t2)));
+    }
 
     vector<char> bitString;
-    queue.tree->printTree(bitString);
+    q.top().tree->printTree(bitString);
     
     return 0;
 }
@@ -44,29 +60,4 @@ unordered_map<char, int> letterCounter(string input) {
 
     // Return the map containing character counts
     return letterMap;
-}
-
-// Function to create and merge trees
-TreeWrapper createTrees(const unordered_map<char, int>& letterCounted){
-
-    priority_queue<TreeWrapper> q;
-
-    // Create base trees and put them in a queue
-    for(const auto & pair: letterCounted) {
-        q.push(TreeWrapper(new Tree(pair.second, pair.first)));
-    }
-
-    while (q.size() > 1) {
-        Tree* t1 = q.top().tree; 
-        q.pop();
-        Tree* t2 = q.top().tree;
-        q.pop();
-
-        //Merge first and second spot in queue
-        int weight = t1->getWeight() + t2->getWeight();
-        q.push(TreeWrapper(new Tree(weight, t1, t2)));
-    }
-
-    
-    return q.top();
 }
